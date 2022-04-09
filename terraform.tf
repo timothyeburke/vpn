622f18ada9ce8211b2cb214f8546856250a2cd09
Tim Burke <tim.burke@nytimes.com>
2022-04-09 19:53:43 -0400

    Update AWS provider version to 4.x

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
   backend "s3" {
     bucket = "vpn-tf-state"
     key    = "vpn-tf-state"
